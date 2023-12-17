1903c42f9f829c79184395ef0c42fb2d85374003
PaolaDaft <andrepaolagd@gmail.com>
2023-12-17 08:00:35 -0600

    Adding code tto use FastApi and Uvicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,15 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact')
+def get_list():
+    return{'name':'Pao'}
 
 def run():
     store.get_categories()
